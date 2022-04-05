c5bf5b39c27a577652674dd7da01f533de8c9967
naveed <naveedqamar6500@gmail.com>
2022-04-05 20:42:00 +0500

    Changes Made

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import cv2
 
 # Load the cascade
-face_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 # Read the input image
 img = cv2.imread('test.jpeg')
